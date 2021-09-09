40cc2b9c414dbe8e7de37709bfaa7ced74dc5d6f
maxxburn <tarkaklavio@gmail.com>
2021-09-09 13:01:23 +0200

    Added tilemaps and camera following character

---
+++ Assets/Exploration Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+ 
+    public Transform player;
+    private Vector3 offset;
+
+    // Update is called once per frame
+
+    private void Start() {
+        
+    }
+    void Update () 
+    {
+        // Camera follows the player with specified offset position
+        transform.position = new Vector3 (player.position.x + offset.x, player.position.y + offset.y, this.transform.position.z); 
+    }
+}
