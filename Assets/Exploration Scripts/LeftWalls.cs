8effd02306977b2116b1d501b7141697396a9d83
MaxxBurn <tarkaklavio@gmail.com>
2021-09-17 19:51:03 +0200

    Changes to walls and scripts

---
+++ Assets/Exploration Scripts/LeftWalls.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftWalls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Character player;
+    private Vector2 wallPosition;
+    void Start()
+    {
+        wallPosition = this.transform.position;
+        player = GameObject.FindObjectOfType<Character>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate() {
+        if (player.Position.x - 0.35 <= wallPosition.x)
+        {
+            Debug.Log("Monkey");
+        }
+    }
+}
