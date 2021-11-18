01e9e902b782e8819d96924251aae1baf3376e8b
maxxburn <tarkaklavio@gmail.com>
2021-11-18 11:30:54 +0100

    Changes to Scriptable Objects

--- Assets/EffectS/EffectScripts/EffectManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using System.IO;
-using System;
-using UnityEngine;
-
-public class EffectManager : MonoBehaviour
-{
-    
-}
--- Assets/EffectScriptableObject.cs
+++ Assets/EffectScriptableObject.cs
@@ -6,6 +6,61 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="effects", menuName = "Effect")]
 public class EffectScriptableObject : ScriptableObject 
 {
+    public virtual string Description{ get; set; }
+}
+
+[CreateAssetMenu(fileName ="effects", menuName = "Lifesteal Effect")]
+public class LifestealScriptableObject : EffectScriptableObject
+{
+    public override string Description
+    {
+        get
+        {
+            return Description;
+        }
+        set
+        {
+            value = "You are able to steal life from your opponent based on how much damage you deal.";
+        }
+    }
+    
+
+    public void PastAttack()
+    {   
+
+    }
+}
+
+[CreateAssetMenu(fileName ="effects", menuName = "Shield Effect")]
+public class ShieldScriptableObject : EffectScriptableObject
+{
     public string description;
     public int cooldown;
+    
+    public void PreAttack()
+    {
+
+    }
+}
+
+[CreateAssetMenu(fileName ="effects", menuName = "Crit Chance Effect")]
+public class CritChanceScriptableObject : EffectScriptableObject
+{
+    public string description;
+
+    public void PreAttack()
+    {
+
+    }
+}
+
+[CreateAssetMenu(fileName ="effects", menuName = "Shield Effect")]
+public class AttackUpScriptableObject : EffectScriptableObject
+{
+    public string description;
+
+    public void PreAttack()
+    {
+
+    }
 }
